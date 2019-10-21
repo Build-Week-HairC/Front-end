8c5658251dbc396c0eb9d9545e4ef18fc8bef0b2
John Wells <molekewl2017@gmail.com>
2019-10-21 14:25:37 -0700

    add routing

--- med/src/App.js
+++ med/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FormikSignupForm from './components/FormikSignupForm';
 
-
-
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <FormikSignupForm />
-    </div>
+    <Router>
+      <div className="App">
+        <FormikSignupForm />
+        {/*
+          line16: component={Login}
+          line17 component={MedcabinetPage} for privateRoute
+        */}
+        <Route exact path='/' />
+        <PrivateRoute path='/medcabinet' />
+      </div>
+    </Router>
   );
 }
 
