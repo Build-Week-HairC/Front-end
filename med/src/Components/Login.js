02d785a19e2b4f6c313ff81ff1fde28e4e942f97
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-21 12:14:13 -0700

    created component files

---
+++ med/src/Components/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function LoginPage() {
+
+
+
+
+    return (
+        <section className="login-page">
+            <button>Login</button>
+            <button>Sign up</button>
+
+
+
+        </section>
+
+
+
+    )
+}
