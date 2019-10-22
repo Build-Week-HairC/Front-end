ccf1074966fed721e915c66d929299a4607f9543
John Wells <molekewl2017@gmail.com>
2019-10-22 13:25:17 -0700

    route to card

--- med/src/App.js
+++ med/src/App.js
@@ -4,6 +4,7 @@ import FormikSignupForm from './components/FormikSignupForm';
 
 import MedCabinet from './components/MedCabinet/MedCabinet';
 import Login from './components/Login';
+import MedCard from './components/MedCard/MedCard';
 
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
@@ -15,6 +16,7 @@ function App() {
         <FormikSignupForm />
         <Route exact path='/' component={Login}/>
         <PrivateRoute path='/medcabinet' component={MedCabinet} />
+        <Route exact path='/medcard' component={MedCard}/>
       </div>
     </Router>
   );
