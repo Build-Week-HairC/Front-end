f8d7ca412c10219073e697f1feb60d5901457099
zacharybaca <baca.zach@icloud.com>
2019-10-24 14:47:36 -0500

    added links to footer

--- med/src/Components/Footer/Footer.js
+++ med/src/Components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div className="footer">
             <nav className="nav">
-                <a className="anchorFooter" href="#">Home</a> | <a className="anchorFooter" href="#">Dashboard</a> | <a className="anchorFooter" href="#">About Us</a>
+                <Link to="/">Home</Link> | <Link to="/dashboard">Dashboard</Link> | <Link>About Us</Link>
             </nav>
             <h5 className="copyright">Copyright Med Cabinet</h5>
         </div>
