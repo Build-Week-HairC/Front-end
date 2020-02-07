b5fd284d10d327e9970ad3b3ba7b39f67b0f047d
Braden <fish2355@hotmail.com>
2020-02-07 13:15:54 -0700

    commit

--- med/src/App.js
+++ med/src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SignupTest from "./components/FormikSignupForm";
-import FormikQuestionForm from "./components/Questionnaire";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignupTest from "./Components/FormikSignupForm";
+import FormikQuestionForm from "./Components/Questionnaire";
 
-import MedCabinet from "./components/MedCabinet/MedCabinet";
+import MedCabinet from "./Components/MedCabinet/MedCabinet";
 
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-
-import LoginTest from './components/LoginTest';
-import MedCard from './components/MedCard/MedCard';
-import SavedStrains from './components/SavedStrains';
+import LoginTest from "./Components/LoginTest";
+import MedCard from "./Components/MedCard/MedCard";
+import SavedStrains from "./Components/SavedStrains";
 import styled from "styled-components";
 
 function App() {
-  
   const AppWrapper = styled.div`
     .app-wrapper {
       display: flex;
@@ -29,23 +27,21 @@ function App() {
 
   return (
     <Router>
-      
       <div className="App">
         <Header />
         <AppWrapper>
           <div className="app-wrapper">
-        <Route path='/signup' component={SignupTest} />
-        <Route path='/questionnaire' component={FormikQuestionForm} />
-        <Route exact path='/' component={LoginTest}/>
-        <PrivateRoute path='/medcabinet' component={MedCabinet} />
-        <Route exact path='/medcard' component={MedCard}/>
-        <Route exact path='/favorites' component={SavedStrains} />
-        </div>
+            <Route path="/signup" component={SignupTest} />
+            <Route path="/questionnaire" component={FormikQuestionForm} />
+            <Route exact path="/" component={LoginTest} />
+            <PrivateRoute path="/medcabinet" component={MedCabinet} />
+            <Route exact path="/medcard" component={MedCard} />
+            <Route exact path="/favorites" component={SavedStrains} />
+          </div>
         </AppWrapper>
-        
+
         <Footer />
       </div>
-      
     </Router>
   );
 }
--- med/src/redux/actions/index.js
+++ med/src/redux/actions/index.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import { axiosWithAuth } from '../../components/utils/AxiosWithAuth';
+import axios from "axios";
+import { axiosWithAuth } from "../../Components/utils/AxiosWithAuth";
 
 // action types
-export const START_FETCHING = 'START_FETCHING';
-export const FETCH_SUCCESS = 'FETCH_SUCCESS';
-export const FETCH_FAILURE = 'FETCH_FAILURE';
-export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
-export const REMOVE_STRAIN = 'REMOVE_STRAIN';
-export const ADD_STRAIN = 'ADD_STRAIN';
+export const START_FETCHING = "START_FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+export const REMOVE_STRAIN = "REMOVE_STRAIN";
+export const ADD_STRAIN = "ADD_STRAIN";
 
 // action creator0
 export const dsData = () => dispatch => {
   dispatch({ type: START_FETCHING });
 
   axios
-    .get('https://medcabinet.herokuapp.com/api/ds/sativa')
+    .get("https://medcabinet.herokuapp.com/api/ds/sativa")
     .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
-    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }))
-}
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }));
+};
 
 export const fetchData = () => dispatch => {
   dispatch({ type: START_FETCHING });
@@ -25,34 +25,36 @@ export const fetchData = () => dispatch => {
   axiosWithAuth()
     .get(`https://medcabinet.herokuapp.com/strains/strains/user`)
     .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
-    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }))
-}
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }));
+};
 
-export const saveStrain = (strain) => dispatch => {
+export const saveStrain = strain => dispatch => {
   dispatch({ type: START_FETCHING });
 
   axiosWithAuth()
-    .post('https://medcabinet.herokuapp.com/strains/strain', strain)
+    .post("https://medcabinet.herokuapp.com/strains/strain", strain)
     .then(res => dispatch({ type: ADD_STRAIN, payload: strain }))
-    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }))
-}
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }));
+};
 
-export const deleteStrain = (strain) => dispatch => {
+export const deleteStrain = strain => dispatch => {
   dispatch({ type: START_FETCHING });
 
   axiosWithAuth()
-    .delete(`https://medcabinet.herokuapp.com/strains/strain`, { data: {'strain': strain.strain}})
+    .delete(`https://medcabinet.herokuapp.com/strains/strain`, {
+      data: { strain: strain.strain }
+    })
     .then(dispatch({ type: REMOVE_STRAIN, payload: strain }))
     .then(dispatch(fetchData()))
-    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }))
-}
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err }));
+};
 
 export const removeStrain = strain => ({
   type: REMOVE_STRAIN,
   payload: strain
-})
+});
 
 export const addStrain = strain => ({
   type: ADD_STRAIN,
   payload: strain
-})
+});
